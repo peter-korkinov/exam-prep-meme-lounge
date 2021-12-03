bd507c3f5d457c517ce473fb32cf5c96659aaa39
Peter <73311957+peter-korkinov@users.noreply.github.com>
2021-12-03 15:11:23 +0200

    initial setting up of api modules and some css touch-ups to make page appear in dark mode

---
+++ src/api/data.js
@@ -0,0 +1,94 @@
+import * as api from './api.js';
+
+
+const login = api.login;
+const register = api.register;
+const logout = api.logout;
+
+const pageSize = 4;
+
+const endpoints = {
+    allProducts: '/data/catalog?pageSize=4&offset=',
+    count: '/data/catalog?',
+    productById: '/data/catalog/',
+    productsOfUser: (userId) => `/data/catalog/?where=_ownerId%3D%22${userId}%22&pageSize=4&offset=`,
+    productsOfUserCount: (userId) =>  `/data/catalog/?where=_ownerId%3D%22${userId}%22`,
+    create: '/data/catalog',
+    edit: '/data/catalog/',
+    delete: '/data/catalog/'
+};
+
+async function getPagesCount(search) {
+    let url = endpoints.count;
+
+    if (search) {
+        url += 'where=' + encodeURIComponent(`make LIKE "${search}"`) + '&count';
+    } else {
+        url += 'count';
+    }
+
+    return api.get(url);
+}
+
+async function getAllProducts(page, search) {
+    let url = endpoints.allProducts + (page - 1) * pageSize;
+
+    if (search) {
+        url += '&where=' + encodeURIComponent(`make LIKE "${search}"`);
+    }
+
+    return api.get(url);
+}
+
+async function getPagesCountOfUserProducts(id, search) {
+    let url = endpoints.productsOfUserCount(id);
+
+    if (search) {
+        url += '&where=' + encodeURIComponent(`make LIKE "${search}"`) + '&count';
+    } else {
+        url += '&count';
+    }
+
+    return api.get(url);
+}
+
+async function getAllProductsOfUserId(page, id, search) {
+    let url = endpoints.productsOfUser(id) + (page - 1) * pageSize
+
+    if (search) {
+        url += '&where=' + encodeURIComponent(`make LIKE "${search}"`);
+    }
+
+    return api.get(url);
+}
+
+async function getProductById(id) {
+    return api.get(endpoints.productById + id);
+}
+
+async function createProduct(data) {
+    return api.post(endpoints.create, data);
+}
+
+async function editProduct(id, data) {
+    return api.put(endpoints.edit + id, data);
+}
+
+async function deleteProductById(id) {
+    return api.del(endpoints.delete + id);
+}
+
+export {
+    login,
+    register,
+    logout,
+    getProductById,
+    getAllProducts,
+    createProduct,
+    editProduct,
+    deleteProductById,
+    getAllProductsOfUserId,
+    getPagesCount,
+    getPagesCountOfUserProducts,
+    pageSize
+}
---
+++ src/util.js
@@ -0,0 +1,59 @@
+import {deleteProductById, getProductById} from "./api/data.js";
+// import {showModal} from "./common/modal.js";
+
+function getUserData() {
+    return JSON.parse(sessionStorage.getItem('userData'));
+}
+
+function setUserdata(data) {
+    sessionStorage.setItem('userData', JSON.stringify(data));
+}
+
+function clearUserData() {
+    sessionStorage.removeItem('userData');
+}
+
+function isLogged() {
+    return !(sessionStorage.getItem('userData') === null);
+}
+
+async function loadProduct(ctx, next) {
+    ctx.productPromise = getProductById(ctx.params.id);
+    next();
+}
+
+// async function deleteProduct(ctx) {
+//     showModal('Are you sure you want to delete this item?', onSelect);
+//
+//     async function onSelect(choice) {
+//         if (choice) {
+//             await deleteProductById(ctx.params.id);
+//             ctx.page.redirect('/home');
+//         }
+//     }
+//
+// }
+
+function isValidUrl(str) {
+    let a  = document.createElement('a');
+    a.href = str;
+    return (a.host && a.host !== window.location.host);
+}
+
+function parseQueryString(string) {
+    return string
+        .split('&')
+        .map(p => p.split('='))
+        .reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
+}
+
+export {
+    getUserData,
+    setUserdata,
+    clearUserData,
+    isLogged,
+    loadProduct,
+    // deleteProduct,
+    isValidUrl,
+    parseQueryString
+};
