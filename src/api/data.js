1eb6442b045c772acef8226bbe00ebae8efd572e
Peter <73311957+peter-korkinov@users.noreply.github.com>
2021-12-05 20:29:37 +0200

    some tidying up of the code

--- src/api/data.js
+++ src/api/data.js
@@ -5,48 +5,20 @@ const login = api.login;
 const register = api.register;
 const logout = api.logout;
 
-const pageSize = 4;
-
 const endpoints = {
     allRecords: '/data/memes?sortBy=_createdOn%20desc',
-    // allRecordsCount: '/data/catalog?',
     recordById: '/data/memes/',
     recordsOfUser: (userId) => `/data/memes?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`,
-    // productsOfUserCount: (userId) =>  `/data/catalog/?where=_ownerId%3D%22${userId}%22`,
     create: '/data/memes',
     edit: '/data/memes/',
     delete: '/data/memes/'
 };
-
-// async function getPagesCount(search) {
-//     let url = endpoints.allRecordsCount;
-//
-//     if (search) {
-//         url += 'where=' + encodeURIComponent(`make LIKE "${search}"`) + '&count';
-//     } else {
-//         url += 'count';
-//     }
-//
-//     return api.get(url);
-// }
 
 async function getAllRecords() {
     let url = endpoints.allRecords;
 
     return api.get(url);
 }
-
-// async function getPagesCountOfUserProducts(id, search) {
-//     let url = endpoints.productsOfUserCount(id);
-//
-//     if (search) {
-//         url += '&where=' + encodeURIComponent(`make LIKE "${search}"`) + '&count';
-//     } else {
-//         url += '&count';
-//     }
-//
-//     return api.get(url);
-// }
 
 async function getAllRecordsOfUserId(id) {
     return api.get(endpoints.recordsOfUser(id));
@@ -77,8 +49,5 @@ export {
     createRecord,
     editRecordById,
     deleteRecordById,
-    getAllRecordsOfUserId,
-    // getPagesCount,
-    // getPagesCountOfUserProducts,
-    pageSize
+    getAllRecordsOfUserId
 }
--- src/common/util.js
+++ src/common/util.js
@@ -21,23 +21,10 @@ async function loadRecord(ctx, next) {
     next();
 }
 
-async function deleteProduct(ctx) {
-    // showModal('Are you sure you want to delete this item?', onSelect);
-    //
-    // async function onSelect(choice) {
-    //     if (choice) {
-    //         await deleteProductById(ctx.params.id);
-    //         ctx.page.redirect('/home');
-    //     }
-    // }
-
-}
-
 export {
     getUserData,
     setUserdata,
     clearUserData,
     isLogged,
-    loadRecord,
-    deleteProduct
+    loadRecord
 };
